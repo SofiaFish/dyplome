bbe2bf3233a4cbb1f9285fce83b5a9c1aa0b68b9
Admin <admin@Admins-MacBook-Air.local>
2019-11-19 16:56:35 +0200

    new_activity

---
+++ app/src/main/java/com/example/dyplome/BeckScaleActivity.java
@@ -0,0 +1,14 @@
+package com.example.dyplome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeckScaleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beck_scale);
+    }
+}
